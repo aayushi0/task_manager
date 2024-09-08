e3e939ee4bf31bd7c24350a1efc4990574debd37
aayushi0 <99340905+aayushi0@users.noreply.github.com>
2024-09-08 23:53:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://task-manager-backend-opsr.onrender.com",
         changeOrigin: true,
       },
     },
